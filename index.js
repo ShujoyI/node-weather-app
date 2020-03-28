0e8480ede89e3f7b8a2d4d983db6ecad9e00a2bc
ShujoyI <shujoyi@gmail.com>
2020-03-28 14:22:17 -0700

    Update index.js
    used node-fetch instead of deprecated request to get weather info

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const request = require('request');
+const fetch = require('node-fetch');
 
 let apiKey = '9dc9bda53cd4556e407c38cd07a0dff8';
 let city = 'portland';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}';
 
-request(url, function (err, response, body) {
-    if (err) {
-        console.log('error:', error);
-    }
-    else {
-        console.log('body:', body);
-    }
-});
+async function response() {
+    console.log('Wowie');
+}
+
+fetch(url)
+    .then(res => res.text())
+    .then(body => console.log(body));
