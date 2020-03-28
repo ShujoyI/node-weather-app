c8d1b1f0ae16f29f7b2831bfb569b51499920421
ShujoyI <shujoyi@gmail.com>
2020-03-28 14:24:49 -0700

    Update index.js
    updated URL

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 let apiKey = '9dc9bda53cd4556e407c38cd07a0dff8';
 let city = 'portland';
-let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}';
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}';
 
 async function response() {
     console.log('Wowie');
