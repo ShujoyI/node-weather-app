9f9d5c0b7ca4386557912721c906fc18f35137f2
ShujoyI <shujoyi@gmail.com>
2020-03-28 14:05:11 -0700

    updated index.js
    Index.js makes a call to request current weather
    
    Downloaded "Request" npm module

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-ECHO is on.
+const request = require('request');
+
+let apiKey = '9dc9bda53cd4556e407c38cd07a0dff8';
+let city = 'portland';
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}';
+
+request(url, function (err, response, body) {
+    if (err) {
+        console.log('error:', error);
+    }
+    else {
+        console.log('body:', body);
+    }
+});
